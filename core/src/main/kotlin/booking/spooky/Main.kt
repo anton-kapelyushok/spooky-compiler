13b042e3d4371f6d56b028bf44bd7b537f871cbc
Anton Kapelyushok <anton.kapelyushok@booking.com>
2023-02-07 15:14:30 +0100

    update cmd arguments

--- core/src/main/kotlin/booking/spooky/Main.kt
+++ core/src/main/kotlin/booking/spooky/Main.kt
@@ -23,7 +23,7 @@ import kotlin.io.path.absolutePathString
 
 // https://sourcegraph.com/github.com/georgewfraser/java-language-server/-/blob/src/main/java/org/javacs/SourceFileManager.java
 
-// --add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED
+// -ea --add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED
 fun main() {
 
 
